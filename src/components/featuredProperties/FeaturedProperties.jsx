ee6e607f7431558dcfe766d3c86c83222d18642a
Jubyer Ahmed Shezan <84038649+jashezan@users.noreply.github.com>
2023-03-21 15:27:04 +0600

    Added feature Projects

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import fpList from "../../data/fpList"
 import "./featuredProperties.scss";
 
 const FeaturedProperties = () => {
   return (
-    <div>FeaturedProperties</div>
+    <div className='fp'>
+      {fpList.map((item, index) => {
+        return (
+          <div key={index} className="fpItem">
+            <img src={item.photos} alt={item.name} className="fpImg" />
+            <span className="fpName"> {item.name} </span>
+            <span className="fpCity"> {item.city} </span>
+            <span className="fpPrice"> ${item.cheapestPrince} </span>
+            <div className="fpRating">
+              <button>{item.rating.score}</button>
+              <span> {item.rating.comment} </span>
+            </div>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
---
+++ src/data/fpList.js
@@ -0,0 +1,44 @@
+const fpList = [
+  {
+    photos:
+      "https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Banglo House",
+    city: "Dhaka",
+    cheapestPrince: "90",
+    rating: { score: 5, comment: "Excellent" },
+  },
+  {
+    photos:
+      "https://images.unsplash.com/photo-1631049552057-403cdb8f0658?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdGVsJTIwcm9vbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Vuture House",
+    city: "Sylhet",
+    cheapestPrince: "40",
+    rating: { score: 5, comment: "Excellent" },
+  },
+  {
+    photos:
+      "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWwlMjByb29tfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Hauting House",
+    city: "Khulna",
+    cheapestPrince: "50",
+    rating: { score: 5, comment: "Excellent" },
+  },
+  {
+    photos:
+      "https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG90ZWwlMjByb29tfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Crime Scene House",
+    city: "Rajshahi",
+    cheapestPrince: "20",
+    rating: { score: 5, comment: "Excellent" },
+  },
+  {
+    photos:
+      "https://images.unsplash.com/photo-1630660664869-c9d3cc676880?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGhvdGVsJTIwcm9vbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Banglo House",
+    city: "Cumilla",
+    cheapestPrince: "10",
+    rating: { score: 5, comment: "Excellent" },
+  },
+];
+
+export default fpList;
